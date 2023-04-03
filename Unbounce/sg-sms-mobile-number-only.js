51b99f87dd3cf799c70c05bba17f1f357b3f50c9
Jaime Guevarra Jr <jaimeguevarra22@gmail.com>
2023-04-03 12:58:02 +0800

    Create sg-sms-mobile-number-only.js
    mobile only for SG SMS feature

---
+++ Unbounce/sg-sms-mobile-number-only.js
@@ -0,0 +1,30 @@
+ // on blur: validate
+  input.addEventListener("blur", function () {
+    reset();
+    if (input.value.trim()) {
+      if (iti.isValidNumber()) { 
+        if(iti.getSelectedCountryData().iso2 === "au" && iti.getNumberType() === 1){ // In AU number must be mobile only. ref https://github.com/jackocnr/intl-tel-input/blob/master/src/js/utils.js#L119
+          input.classList.add("error");
+          var errorCode = iti.getValidationError();
+          errorMsg.innerHTML = errorMap[5];
+          errorMsg.classList.remove("hide");
+          $("#" + phoneField)
+            .closest(".lp-pom-form-field")
+            .addClass("fieldError");
+        }else{ 
+          validMsg.classList.remove("hide");
+          $("#" + phoneField)
+            .closest(".lp-pom-form-field")
+            .removeClass("fieldError");
+        }
+      } else {
+        input.classList.add("error");
+        var errorCode = iti.getValidationError();
+        errorMsg.innerHTML = errorMap[errorCode];
+        errorMsg.classList.remove("hide");
+        $("#" + phoneField)
+          .closest(".lp-pom-form-field")
+          .addClass("fieldError");
+      }
+    }
+  });
