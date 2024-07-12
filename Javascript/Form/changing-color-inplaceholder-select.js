94f57c8a7c124d020a5f6b6d4d1edb38b4682a0a
Jaime Guevarra Jr <jaimeguevarra22@gmail.com>
2024-07-12 14:49:22 +0800

    Update changing-color-inplaceholder-select.js

--- Javascript/Form/changing-color-inplaceholder-select.js
+++ Javascript/Form/changing-color-inplaceholder-select.js
@@ -1,7 +1,10 @@
 <script type="text/javascript">
-  $(document).ready(function(){
-    $(this).addClass('--placeholder');
-    $('select[name="timeline_to_purchase"]').change(function(){
+ $(document).ready(function(){
+    $('select[name="timeline_to_purchase"]').addClass('--placeholder');
+    $('select[name="timeline_to_purchase"]').focus(function(){
+      $('select[name="timeline_to_purchase"]').removeClass('--placeholder');
+    });
+    $('select[name="timeline_to_purchase"]').blur(function(){
       $(this).removeClass('--placeholder');
       if($(this).val() == '' || $(this).val() == null){
         $(this).addClass('--placeholder');
